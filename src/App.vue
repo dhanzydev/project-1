bba624519f0f70e76186faa1658072e48eda00d4
Dipta Dhananjaya <madedipta88@gmail.com>
2022-11-29 22:34:02 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <RouterLink to="/">Home</RouterLink>
+
+  <RouterView />
+</template>
+
+<script>
+import { RouterLink, RouterView } from "vue-router";
+
+export default {
+  name: "App",
+};
+</script>
