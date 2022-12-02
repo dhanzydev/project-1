859d8276ddf8daf330f6ecef3991d961202d97db
Dipta Dhananjaya <madedipta88@gmail.com>
2022-12-02 09:36:33 +0800

    custom color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      black: "#222831",
+      gray: "#393e46",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
